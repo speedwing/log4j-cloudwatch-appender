08c4d132205b12a00819c931a03952387c51fe7f
Giovanni Gargiulo <ggargiulo@gilt.com>
2016-12-01 19:37:34 +0000

    Migrated to a version with a blocking queue in the middle to avoid flooding cloudwatch logs and getting throttled

--- src/main/java/com/github/speedwing/log4j/cloudwatch/appender/CloudwatchAppender.java
+++ src/main/java/com/github/speedwing/log4j/cloudwatch/appender/CloudwatchAppender.java
@@ -1,18 +1,25 @@
 package com.github.speedwing.log4j.cloudwatch.appender;
 
+
 import com.amazonaws.services.logs.AWSLogsClient;
 import com.amazonaws.services.logs.model.*;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static java.util.stream.Collectors.toList;
+
 public class CloudwatchAppender extends AppenderSkeleton {
+
+    private LinkedBlockingQueue<LoggingEvent> loggingEventsQueue;
 
     private AWSLogsClient awsLogsClient;
 
@@ -21,6 +28,12 @@ public class CloudwatchAppender extends AppenderSkeleton {
     private String logGroupName;
 
     private String logStreamName;
+
+    private int queueLength = 1024;
+
+    private int messagesBatchSize = 128;
+
+    private AtomicBoolean cloudwatchAppenderInitialised = new AtomicBoolean(false);
 
     public CloudwatchAppender() {
         super();
@@ -42,33 +55,66 @@ public class CloudwatchAppender extends AppenderSkeleton {
         this.logStreamName = logStreamName;
     }
 
+    public void setQueueLength(int queueLength) {
+        this.queueLength = queueLength;
+    }
+
+    public void setMessagesBatchSize(int messagesBatchSize) {
+        this.messagesBatchSize = messagesBatchSize;
+    }
+
     @Override
     protected void append(LoggingEvent event) {
+        if (cloudwatchAppenderInitialised.get()) {
+            loggingEventsQueue.offer(event);
+        } else {
+            // just do nothing
+        }
+    }
 
-        String lineEvent = layout.format(event);
+    private synchronized void sendMessages() {
+        LoggingEvent polledLoggingEvent;
 
-        InputLogEvent inputLogEvent = new InputLogEvent().withTimestamp(new Date().getTime()).withMessage(lineEvent);
-        List<InputLogEvent> list = new ArrayList<>();
-        list.add(inputLogEvent);
-        PutLogEventsRequest putLogEventsRequest = new PutLogEventsRequest(
-                logGroupName,
-                logStreamName, list);
+        List<LoggingEvent> loggingEvents = new ArrayList<>();
 
         try {
-            putLogEventsRequest.setSequenceToken(lastSequenceToken.get());
-            PutLogEventsResult result = awsLogsClient.putLogEvents(putLogEventsRequest);
-            lastSequenceToken.set(result.getNextSequenceToken());
-        } catch (InvalidSequenceTokenException invalidSequenceTokenException) {
-            putLogEventsRequest.setSequenceToken(invalidSequenceTokenException.getExpectedSequenceToken());
-            PutLogEventsResult result = awsLogsClient.putLogEvents(putLogEventsRequest);
-            lastSequenceToken.set(result.getNextSequenceToken());
+
+            while ((polledLoggingEvent = loggingEventsQueue.poll()) != null && loggingEvents.size() <= messagesBatchSize) {
+                loggingEvents.add(polledLoggingEvent);
+            }
+
+            List<InputLogEvent> inputLogEvents = loggingEvents.stream()
+                    .map(loggingEvent -> new InputLogEvent().withTimestamp(loggingEvent.getTimeStamp()).withMessage(layout.format(loggingEvent)))
+                    .collect(toList());
+
+            if (!inputLogEvents.isEmpty()) {
+
+                PutLogEventsRequest putLogEventsRequest = new PutLogEventsRequest(
+                        logGroupName,
+                        logStreamName,
+                        inputLogEvents);
+
+                try {
+                    putLogEventsRequest.setSequenceToken(lastSequenceToken.get());
+                    PutLogEventsResult result = awsLogsClient.putLogEvents(putLogEventsRequest);
+                    lastSequenceToken.set(result.getNextSequenceToken());
+                } catch (InvalidSequenceTokenException invalidSequenceTokenException) {
+                    putLogEventsRequest.setSequenceToken(invalidSequenceTokenException.getExpectedSequenceToken());
+                    PutLogEventsResult result = awsLogsClient.putLogEvents(putLogEventsRequest);
+                    lastSequenceToken.set(result.getNextSequenceToken());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
     @Override
     public void close() {
-
+        while (!loggingEventsQueue.isEmpty()) {
+            this.sendMessages();
+        }
     }
 
     @Override
@@ -80,7 +126,28 @@ public class CloudwatchAppender extends AppenderSkeleton {
     public void activateOptions() {
         super.activateOptions();
         this.awsLogsClient = new AWSLogsClient();
-        initializeCloudwatchResources();
+        loggingEventsQueue = new LinkedBlockingQueue<>(queueLength);
+        try {
+            initializeCloudwatchResources();
+            initCloudwatchDaemon();
+            cloudwatchAppenderInitialised.set(true);
+        } catch (Exception e) {
+            Logger.getRootLogger().error("Could not initialise Cloudwatch Logs for LogGroupName: " + logGroupName + " and LogStreamName: " + logStreamName, e);
+        }
+    }
+
+    private void initCloudwatchDaemon() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    if (loggingEventsQueue.size() > 0) {
+                        sendMessages();
+                    }
+                    Thread.currentThread().sleep(20L);
+                } catch (InterruptedException e) {
+                }
+            }
+        }).start();
     }
 
     private void initializeCloudwatchResources() {
@@ -110,6 +177,7 @@ public class CloudwatchAppender extends AppenderSkeleton {
                 .findFirst();
 
         if (!logStreamOptional.isPresent()) {
+            Logger.getLogger(this.getClass()).info("About to create LogStream: " + logStreamName + "in LogGroup: " + logGroupName);
             CreateLogStreamRequest createLogStreamRequest = new CreateLogStreamRequest().withLogGroupName(logGroupName).withLogStreamName(logStreamName);
             awsLogsClient.createLogStream(createLogStreamRequest);
         }
